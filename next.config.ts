ceba0ebfa3b60d5a4ef9e104e1fa4a63f56af9f1
EthanPKittell <ekittell20@gmail.com>
2025-11-30 19:30:16 -0500

    Got home page showing!

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+        {
+            source: '/',
+            destination: '/home',
+            permanent: true,
+        }
+    ]
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
